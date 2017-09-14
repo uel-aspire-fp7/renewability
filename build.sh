295963a914b253c0d8639b83ab83c393831e1fca
babrath <bert.abrath@UGent.be>
2017-09-14 17:53:13 +0200

    Adapted build script so it takes a target directory and builds the objects there.

--- build.sh
+++ build.sh
@@ -7,22 +7,26 @@ BASE_DIRECTORY=$(dirname $0)
 MANAGER_PLATFORM=serverlinux
 
 cd ${BASE_DIRECTORY}
+TARGET_DIRECTORY=$1
 
 echo "Copying headers files to headers/..."
 
-cp src/client/*.h headers/client/
-cp src/server/*.h headers/server/
+mkdir -p ${TARGET_DIRECTORY}/headers/{client,server}
+cp src/client/*.h ${TARGET_DIRECTORY}/headers/client/
+cp src/server/*.h ${TARGET_DIRECTORY}/headers/server/
 
 echo "Building client-side support object files..."
 
+mkdir -p ${TARGET_DIRECTORY}/logs
 for PLATFORM in linux android serverlinux; do
     cd src/client
-    make -f Makefile.${PLATFORM} clean && make -f Makefile.${PLATFORM} >> ../../logs/client-support.log 2>> ../../logs/client-support.err
+    make -f Makefile.${PLATFORM} clean && make -f Makefile.${PLATFORM} >> ${TARGET_DIRECTORY}/logs/client-support.log 2>> ${TARGET_DIRECTORY}/logs/client-support.err
 
     echo "Copying client-side object file to obj/${PLATFORM}"
 
     cd ../..
-    cp src/client/renewability.{a,o} obj/${PLATFORM}
+    mkdir -p ${TARGET_DIRECTORY}/obj/${PLATFORM}
+    cp src/client/renewability.{a,o} ${TARGET_DIRECTORY}/obj/${PLATFORM}
 done
 
 echo "Building server-side support..."
@@ -35,11 +39,12 @@ fi
 pidof renewability_manager
 
 cd src/server
-make -f Makefile.${MANAGER_PLATFORM} clean && make -f Makefile.${MANAGER_PLATFORM} > ../../logs/server-support.log 2> ../../logs/server-support.err
+make -f Makefile.${MANAGER_PLATFORM} clean && make -f Makefile.${MANAGER_PLATFORM} > ${TARGET_DIRECTORY}/logs/server-support.log 2> ${TARGET_DIRECTORY}/logs/server-support.err
 
 echo "Copying renewability_manager to obj/..."
 
 cd ../..
-cp src/server/renewability_manager obj/${MANAGER_PLATFORM}
+mkdir -p ${TARGET_DIRECTORY}/obj/${MANAGER_PLATFORM}
+cp src/server/renewability_manager ${TARGET_DIRECTORY}/obj/${MANAGER_PLATFORM}
 
 echo "Done. For additional info, please refer to {client,server}-support.{log,err}."
