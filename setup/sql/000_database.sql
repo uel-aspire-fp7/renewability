7497ea6852396375e7630699c73dfa8fa428d7d7
babrath <bert.abrath@UGent.be>
2017-09-19 11:15:54 +0200

    Allow users to connect from hosts other than localhost.

--- setup/sql/000_database.sql
+++ setup/sql/000_database.sql
@@ -1,3 +1,3 @@
 CREATE SCHEMA IF NOT EXISTS `RN_development` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON `RN_development`.* TO 'rn_user'@'localhost' IDENTIFIED BY 'rn_password';
+GRANT ALL PRIVILEGES ON `RN_development`.* TO 'rn_user'@'%' IDENTIFIED BY 'rn_password';
