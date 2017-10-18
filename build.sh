77b20899e14ae7debfb58a783d5f36eccd700ec2
babrath <bert.abrath@UGent.be>
2017-10-18 22:04:28 +0200

    Updated build script to make it more uniform.

--- build.sh
+++ build.sh
@@ -1,50 +1,47 @@
 #!/bin/bash
-
-#set -o errexit
+set -o errexit
 set -o pipefail
+set -o nounset
+#set -o xtrace
 
-BASE_DIRECTORY=$(dirname $0)
-MANAGER_PLATFORM=serverlinux
+# Get the repo and build directories, go to the build directory
+repo_dir=$(dirname $0)
+build_dir=$1
+mkdir -p $build_dir
+cd $build_dir
 
-cd ${BASE_DIRECTORY}
-TARGET_DIRECTORY=$1
+# Create extra symlinks
+ln -s $repo_dir/scripts/ $build_dir
+ln -s $repo_dir/setup/ $build_dir
 
 echo "Copying headers files to headers/..."
 
-mkdir -p ${TARGET_DIRECTORY}/headers/{client,server}
-cp src/client/*.h ${TARGET_DIRECTORY}/headers/client/
-cp src/server/*.h ${TARGET_DIRECTORY}/headers/server/
+mkdir -p ${build_dir}/headers/{client,server}
+ln -s ${repo_dir}/src/client/*.h ${build_dir}/headers/client
+ln -s ${repo_dir}/src/server/*.h ${build_dir}/headers/server
 
 echo "Building client-side support object files..."
 
-mkdir -p ${TARGET_DIRECTORY}/logs
+mkdir -p ${build_dir}/logs
+cd ${repo_dir}/src/client
 for PLATFORM in linux android serverlinux; do
-    cd src/client
-    make -f Makefile.${PLATFORM} clean && make -f Makefile.${PLATFORM} >> ${TARGET_DIRECTORY}/logs/client-support.log 2>> ${TARGET_DIRECTORY}/logs/client-support.err
+    make -f Makefile.${PLATFORM} clean && make -f Makefile.${PLATFORM} >> ${build_dir}/logs/client-support.log 2>> ${build_dir}/logs/client-support.err
 
     echo "Copying client-side object file to obj/${PLATFORM}"
 
-    cd ../..
-    mkdir -p ${TARGET_DIRECTORY}/obj/${PLATFORM}
-    cp src/client/renewability.{a,o} ${TARGET_DIRECTORY}/obj/${PLATFORM}
+    mkdir -p ${build_dir}/obj/${PLATFORM}
+    mv ${repo_dir}/src/client/renewability.{a,o} ${build_dir}/obj/${PLATFORM}
 done
 
 echo "Building server-side support..."
-RN_PID=$(pidof renewability_manager)
+MANAGER_PLATFORM=serverlinux
 
-if [ "${RN_PID}" != "" ]; then
-    kill -9 ${RN_PID}
-fi
-
-pidof renewability_manager
-
-cd src/server
-make -f Makefile.${MANAGER_PLATFORM} clean && make -f Makefile.${MANAGER_PLATFORM} > ${TARGET_DIRECTORY}/logs/server-support.log 2> ${TARGET_DIRECTORY}/logs/server-support.err
+cd ${repo_dir}/src/server
+make -f Makefile.${MANAGER_PLATFORM} clean && make -f Makefile.${MANAGER_PLATFORM} > ${build_dir}/logs/server-support.log 2> ${build_dir}/logs/server-support.err
 
 echo "Copying renewability_manager to obj/..."
 
-cd ../..
-mkdir -p ${TARGET_DIRECTORY}/obj/${MANAGER_PLATFORM}
-cp src/server/renewability_manager ${TARGET_DIRECTORY}/obj/${MANAGER_PLATFORM}
+mkdir -p ${build_dir}/obj/${MANAGER_PLATFORM}
+mv ${repo_dir}/src/server/renewability_manager ${build_dir}/obj/${MANAGER_PLATFORM}
 
 echo "Done. For additional info, please refer to {client,server}-support.{log,err}."
